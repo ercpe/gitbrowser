8980e76e7af2a9d408dd0e1b0517dc6831e1bee3
Johann Schmitz <johann@j-schmitz.net>
2015-01-18 19:19:30 +0100

    Try mime_type if no lexer could be found for the filename

--- src/gitbrowser/templatetags/highlight.py
+++ src/gitbrowser/templatetags/highlight.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+import logging
 
 from django import template
 from pygments import highlight
 from pygments.formatters.html import HtmlFormatter
 from pygments.lexers import get_lexer_for_mimetype, get_lexer_for_filename
+from pygments.util import ClassNotFound
 
 register = template.Library()
 
@@ -13,9 +15,15 @@ def pygments_highlight2(code, mime_type, filename=None):
 	if not code:
 		return ""
 
+	lexer = None
+
 	if mime_type == "text/plain" and filename:
-		lexer = get_lexer_for_filename(filename)
-	else:
+		try:
+			lexer = get_lexer_for_filename(filename)
+		except ClassNotFound as cnf:
+			logging.info("No lexer: %s" % cnf)
+
+	if not lexer:
 		lexer = get_lexer_for_mimetype(mime_type)
 
 	return highlight(code, lexer, HtmlFormatter())
