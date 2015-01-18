ec5f7c1b96a4a1ec6db417f0bf7421171061f419
Johann Schmitz <johann@j-schmitz.net>
2015-01-18 18:46:29 +0100

    Use a tag instead of a filter for syntax highlighting.
    Also, try to guess the correct lexer from the optional filename argument if the mime_type is text/plain.

--- src/gitbrowser/templatetags/highlight.py
+++ src/gitbrowser/templatetags/highlight.py
@@ -3,14 +3,19 @@
 from django import template
 from pygments import highlight
 from pygments.formatters.html import HtmlFormatter
-from pygments.lexers import get_lexer_for_mimetype
+from pygments.lexers import get_lexer_for_mimetype, get_lexer_for_filename
 
 register = template.Library()
 
-@register.filter(name='highlight')
-def pygments_highlight(code, mime_type):
-	lexer = get_lexer_for_mimetype(mime_type)
-	if code:
-		return highlight(code, lexer, HtmlFormatter())
-	else:
+
+@register.simple_tag(name='highlight')
+def pygments_highlight2(code, mime_type, filename=None):
+	if not code:
 		return ""
+
+	if mime_type == "text/plain" and filename:
+		lexer = get_lexer_for_filename(filename)
+	else:
+		lexer = get_lexer_for_mimetype(mime_type)
+
+	return highlight(code, lexer, HtmlFormatter())
