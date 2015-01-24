2c54b426765c92b031ae2b0a5bdb00c40d515d05
Johann Schmitz <johann@j-schmitz.net>
2015-01-24 16:07:33 +0100

    AvatarView:
    - Return 404 if gravatars feature is disabled

--- src/gitbrowser/views/aux.py
+++ src/gitbrowser/views/aux.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import hashlib
-from django.http.response import HttpResponse, HttpResponseRedirect
+from django.http.response import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 from django.views.generic.base import View
 from pygments.formatters.html import HtmlFormatter
 from gitbrowser.conf import config
@@ -13,9 +13,9 @@ def styles(request):
 class ContributerAvatarView(View):
 	def get(self, request, *args, **kwargs):
 		if config.feature_enabled('gravatar'):
-			# https://www.gravatar.com/avatar/ec420bf66201d17d79f5904b5f3e86e3?s=16
 			md5 = hashlib.md5()
 			md5.update(request.GET['email'])
 			size = request.GET.get('size', 64)
 			url = "https://www.gravatar.com/avatar/%s?s=%s" % (md5.hexdigest(), size)
 			return HttpResponseRedirect(url)
+		return HttpResponseNotFound()
