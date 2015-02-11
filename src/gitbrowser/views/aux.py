e4b6fed7741e7cd08279ecf39efcc3901561cc23
Johann Schmitz <johann@j-schmitz.net>
2015-02-11 07:16:57 +0100

    Committer avatar icons
    Fall back to identicons if gravatar support is disabled

--- src/gitbrowser/views/aux.py
+++ src/gitbrowser/views/aux.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 import hashlib
 from django.http.response import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from django.views.generic.base import View
+import pydenticon
 from pygments.formatters.html import HtmlFormatter
 from gitbrowser.conf import config
 
@@ -11,11 +14,37 @@ def styles(request):
 
 
 class ContributerAvatarView(View):
+
 	def get(self, request, *args, **kwargs):
+		email = request.GET['email']
+		size = int(request.GET.get('size', 64))
+
 		if config.feature_enabled('gravatar'):
 			md5 = hashlib.md5()
-			md5.update(request.GET['email'])
-			size = request.GET.get('size', 64)
+			md5.update(email)
 			url = "https://www.gravatar.com/avatar/%s?s=%s" % (md5.hexdigest(), size)
 			return HttpResponseRedirect(url)
-		return HttpResponseNotFound()
+		else:
+			# Set-up a list of foreground colours (taken from Sigil).
+			foreground = [
+							"rgb(45,79,255)",
+							"rgb(254,180,44)",
+							"rgb(226,121,234)",
+							"rgb(30,179,253)",
+							"rgb(232,77,65)",
+							"rgb(49,203,115)",
+							"rgb(141,69,170)"
+			]
+
+			# Set-up a background colour (taken from Sigil).
+			background = "rgb(255,255,255)"
+
+			generator = pydenticon.Generator(5, 5, digest=hashlib.sha1,
+												foreground=foreground)
+			identicon = generator.generate(email, size, size)
+			response = HttpResponse(identicon, content_type='image/png')
+			return response
+
+	@method_decorator(cache_page(24 * 60 * 60))
+	def dispatch(self, request, *args, **kwargs):
+		return super(ContributerAvatarView, self).dispatch(request, *args, **kwargs)
